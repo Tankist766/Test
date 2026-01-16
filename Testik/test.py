615d86aba12c2997f4b3cf1fe013a8961edf38a4
Tankist766 <154876149+Tankist766@users.noreply.github.com>
2026-01-16 17:42:19 +0300

    Update test.py

--- Testik/test.py
+++ Testik/test.py
@@ -1,17 +1,39 @@
-import os
+import telebot, os 
+from telebot import types
 
-puti = []
-def leh():
-    for name in os.listdir('images'):
-         put = f"images\{name}"
-         puti.append(put)
-    return puti
+images = os.listdir("images")
+bot = telebot.TeleBot("8052440771:AAF-u01sX3U3VM02dJOBfI6yQy5_JXZeDTA")
+num = 0
+def board():
+    buttons = types.InlineKeyboardMarkup() 
+    left_button = types.InlineKeyboardButton("<", callback_data="left")
+    right_button = types.InlineKeyboardButton(">", callback_data="right")
+    exit_button = types.InlineKeyboardButton("Закрыть2", callback_data="exit")
+    middle_button =types.InlineKeyboardButton(f"1/{len(images) // 2}", callback_data="exit")
+    buttons.add(left_button, middle_button, right_button)
+    buttons.add(exit_button)
+    return buttons
 
-print(leh)
 
 
-def get_duck_image_url():    
-        url = 'https://random-d.uk/api/random'
-        res = requests.get(url)
-        data = res.json()
-        return data['url']
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+
+@bot.message_handler(commands=['foto'])
+def send_welcome(message):
+    with open(f"images/{images[0]}", "rb") as file:
+        bot.send_photo(message.chat.id, file, reply_markup=board())
+
+@bot.callback_query_handler(func=lambda otvet: True) 
+def callback(otvet):
+    global num
+    if otvet.data == "right":
+        num +=1
+        bot.delete_message(chat_id=otvet.message.chat.id,message_id=otvet.message.message_id)
+        
+        with open(f"images/(images[num])", "rb") as file: 
+            bot.send_photo(message.chat.id, file, reply_markup=board())
+
+
+bot.polling(non_stop=True)
